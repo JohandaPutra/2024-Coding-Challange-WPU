90dbd6570f678639912c0177527e3d7c349fd336
Johanda Putra <Johanda201@gmail.com>
2024-02-19 23:09:54 +0700

    Day 17/366 Coding Challange

---
+++ 17-areaOrPerimeter.js
@@ -0,0 +1,12 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+
+const areaOrPerimeter = function(l , w) {
+    return l == w ? l*l : 2*(l+w) 
+};
+
+
+
+console.log(areaOrPerimeter(6,  10))
