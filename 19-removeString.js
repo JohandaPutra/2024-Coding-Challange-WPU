6e4340ab6f0d775e2561967334a0daf1d7795369
Johanda Putra <Johanda201@gmail.com>
2024-02-19 23:24:39 +0700

    Day 19/366 Coding Challange

---
+++ 19-removeString.js
@@ -0,0 +1,16 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+
+// menggunakan split dan join 
+// function noSpace(x){
+//     return x.split(' ').join('')
+// }
+
+// menggunakan replace
+const noSpace = (x) => x.replace(/\s/g,'')
+
+
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
