cfcafdaaaf9957563fabf8f5b90495ede82568e3
Johanda Putra <Johanda201@gmail.com>
2024-02-25 23:39:49 +0700

    Day 23/366 Coding Challange (7Kyu)

---
+++ 23(+)-trolls.js
@@ -0,0 +1,14 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+
+// menggunakan regex untuk memudahkan untuk mencari vokal 
+function disemvowel(str) {
+    return str.replace(/[aeiou]/gi, '');
+  }
+
+
+
+
+  console.log(disemvowel("This website is for losers LOL!"))
