ec97cac03ad030c395fd762ada2d64a1542aa8ab
Johanda Putra <Johanda201@gmail.com>
2024-02-27 23:51:55 +0700

    Day 25/366 Coding Challange (8Kyu)

---
+++ 25(+)-convertString.js
@@ -0,0 +1,13 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+
+
+// function numberToString(num) {
+//     return num.toString();
+//   }
+
+const numberToString = (num) => num.toString();
+ 
+console.log(numberToString(67));
