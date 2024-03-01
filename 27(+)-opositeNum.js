fd8d0913a4a8bb4b38d2d06f96865aab8b26b7ea
Johanda Putra <Johanda201@gmail.com>
2024-03-01 23:49:38 +0700

    Day 27/366 Coding Challange (8Kyu)

---
+++ 27(+)-opositeNum.js
@@ -0,0 +1,10 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+
+function opposite(number) {
+    return -number
+  }
+
+  console.log(opposite(1))
