35637fb634cca3535b92a2e0ec9462850947cfab
Johanda Putra <Johanda201@gmail.com>
2024-03-05 23:38:02 +0700

    Day 31/366 Coding Challange (8Kyu)

---
+++ 31(+)-smallInt.js
@@ -0,0 +1,10 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+function findSmallestInt(args) {
+      return Math.min(...args)
+    }
+  
+
+console.log(findSmallestInt([78,56,232,12,8]))
