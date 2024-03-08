c6f931a553b463b17d1ca730f4781b18374b6e4a
Johanda Putra <Johanda201@gmail.com>
2024-03-08 23:46:28 +0700

    Day 33/366 Coding Challange (7Kyu)

---
+++ 33(+)-beginerSumArray.js
@@ -0,0 +1,10 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+function getSum(a, b)
+{
+  return (Math.abs( a-b) + 1)*(a+b)/2;
+}
+
+console.log(getSum(1,2))
