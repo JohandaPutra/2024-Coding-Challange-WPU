8156f748376058fdab333603f0bda9de2790ce74
Johanda Putra <Johanda201@gmail.com>
2024-03-09 23:51:34 +0700

    Day 34/366 Coding Challange (8Kyu)

---
+++ 34(+)-evenOrOdd.js
@@ -0,0 +1,9 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+function evenOrOdd(number) {
+  return number % 2  == 0 ? 'Even' : 'Odd'
+}
+
+console.log(evenOrOdd(2));
