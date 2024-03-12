5fb7c95f122905b64fe64d994e64354a76048cae
Johanda Putra <Johanda201@gmail.com>
2024-03-12 14:21:02 +0700

    Day 36/366 Coding Challange (8Kyu)

---
+++ 36(+)-simpleMulti.js
@@ -0,0 +1,9 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+function simpleMultiplication(number) {
+    return number % 2 === 1 ? number *8 : number *9;
+}
+
+console.log(simpleMultiplication(2))
