c1a721e234c8f2d6c4ddaf7dd98fc69f2004805e
Johanda Putra <Johanda201@gmail.com>
2024-03-17 23:54:31 +0700

    Day 41/366 Coding Challange (8Kyu)

---
+++ 41(+)-enoughSpace.js
@@ -0,0 +1,10 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+function enough(cap, on, wait) {
+    return cap-on-wait < 0 ? (cap-on-wait)*-1 : 0;
+  }
+
+
+console.log(enough(10, 5, 5))
