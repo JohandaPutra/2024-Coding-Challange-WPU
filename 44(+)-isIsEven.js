cb816c49cefce21de8bf02ba7443d32de8099688
Johanda Putra <Johanda201@gmail.com>
2024-04-21 22:53:15 +0700

    Day 44/366 Coding Challange (8kyu)

---
+++ 44(+)-isIsEven.js
@@ -0,0 +1,9 @@
+// Rules Coding Challange.
+// 1. Get it done. Selesaikan Tantangan Coding tanpa bantuan 
+// 2. Forget the Past. tidak melihat tantangan sebelumnya sebagai cheatcode. 
+
+function testEven(n) {
+    return n  % 2? false : true;
+}
+
+console.log(testEven(4)); // true
